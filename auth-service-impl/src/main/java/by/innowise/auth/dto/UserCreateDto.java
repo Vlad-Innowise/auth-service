b71402f652ea2c9d8cf4c60a4fe4190f257379cb
Vlad Golubev <holybay1886@gmail.com>
2025-09-07 00:48:12 +0300

    Added validation annotations to the UserCreateDto.

--- auth-service-impl/src/main/java/by/innowise/auth/dto/UserCreateDto.java
+++ auth-service-impl/src/main/java/by/innowise/auth/dto/UserCreateDto.java
@@ -1,5 +1,8 @@
 package by.innowise.auth.dto;
 
+import by.innowise.auth.validation.api.EmailAvailable;
+import by.innowise.auth.validation.api.EnumValidator;
+import by.innowise.internship.security.dto.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -9,6 +12,7 @@ public record UserCreateDto(
         @NotBlank(message = "User email address can't be null")
         @Email(message = "Invalid email address")
         @Size(max = 255, message = "User email can't exceed 255 symbols")
+        @EmailAvailable
         String email,
 
         @NotBlank(message = "User password can't be blank")
@@ -16,6 +20,7 @@ public record UserCreateDto(
         String password,
 
         @NotBlank(message = "User role can't be blank")
+        @EnumValidator(enumClass = Role.class)
         String role
 ) {
 }
