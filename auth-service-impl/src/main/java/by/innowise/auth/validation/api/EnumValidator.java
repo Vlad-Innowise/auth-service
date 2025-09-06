7d6ca5c62b3ea8aa462e42d30c7dfdc0a0d2957d
Vlad Golubev <holybay1886@gmail.com>
2025-09-07 00:47:05 +0300

    Added @EnumValidator validation annotation and impl a validator for Enums.

---
+++ auth-service-impl/src/main/java/by/innowise/auth/validation/api/EnumValidator.java
@@ -0,0 +1,28 @@
+package by.innowise.auth.validation.api;
+
+import by.innowise.auth.validation.validator.CustomEnumValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotNull;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = CustomEnumValidator.class)
+@NotNull
+public @interface EnumValidator {
+
+    Class<? extends Enum<?>> enumClass();
+
+    String message() default "The provided value [%s] should be one of the ENUM values %s";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
