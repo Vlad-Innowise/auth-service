9dff73243997f2d2aa5e5a8616e6e407b489e706
Vlad Golubev <holybay1886@gmail.com>
2025-09-07 17:39:15 +0300

    Added static fromType() method to get TokenType instance.

--- auth-service-impl/src/main/java/by/innowise/auth/service/dto/TokenType.java
+++ auth-service-impl/src/main/java/by/innowise/auth/service/dto/TokenType.java
@@ -2,6 +2,8 @@ package by.innowise.auth.service.dto;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 public enum TokenType {
 
     ACCESS("access"),
@@ -12,5 +14,13 @@ public enum TokenType {
 
     TokenType(String type) {
         this.type = type;
+    }
+
+    public static TokenType fromType(String typeName) {
+        return Arrays.stream(TokenType.values())
+                     .filter(e -> e.getType().equalsIgnoreCase(typeName))
+                     .findFirst()
+                     .orElseThrow(() -> new IllegalArgumentException(
+                             "Haven't found a TokenType for [%s] value".formatted(typeName)));
     }
 }
